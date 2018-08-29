4f2a9edee5679435ea282b3de3c8b10c7f99398e
Alexisreau97 <alexis.reaupro@gmail.com>
2018-08-29 13:48:24 +0200

    exo5

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" # affiche le texte entre " " et ajoute une nouvelle ligne après son exécution.
+puts "Travail : #{10 * 5 * 11}" # affiche le texte entre " " et effectue l'opération entre #{}.
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # affiche le texte entre " " et effectue l'opération entre #{}.
+
+puts "Et en secondes ?" # affiche le texte entre " " et ajoute une nouvelle ligne après son exécution.
+
+puts 10 * 5 * 11 * 60 * 60 # affiche le resultat de l'opération.
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # affiche le texte entre " " et ajoute une nouvelle ligne après son exécution.
+
+puts 3 + 2 < 5 - 7 # analyse si le resultat du premier calcul est inférieur au resulat du deuxième.
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # affiche le texte entre " " et effectue l'opération entre #{}.
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # affiche le texte entre " " et effectue l'opération entre #{}.
+
+puts "Ok, c'est faux alors !" # affiche le texte entre " " et ajoute une nouvelle ligne après son exécution.
+
+puts "C'est drôle ça, faisons-en plus :" # affiche le texte entre " " et ajoute une nouvelle ligne après son exécution.
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # affiche le texte entre " " et compare si 5 est plus grand que -2.
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # affiche le texte entre " " et compare si 5 est supérieur ou égal à -2.
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # affiche le texte entre " " et compare si 5 est inférieur ou égal à -2.
