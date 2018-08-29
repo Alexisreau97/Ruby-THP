992bd2fbc2811f79ade10a50810d489870b7fb67
Alexisreau97 <alexis.reaupro@gmail.com>
2018-08-29 13:57:11 +0200

    exo9

---
+++ exo_09.rb
@@ -0,0 +1,7 @@
+puts "Bonjour, quel est ton prénom ?"
+print "> "
+user_name = gets.chomp
+puts "Et quel est ton nom ?"
+print "> "
+user_firstname = gets.chomp
+puts "Bonjour, #{user_name} #{user_firstname} !"
