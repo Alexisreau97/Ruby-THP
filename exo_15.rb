e24db96e568db2d2ebebe010800266ca65a2eb97
Alexisreau97 <alexis.reaupro@gmail.com>
2018-08-30 15:20:50 +0200

    exo15

---
+++ exo_15.rb
@@ -0,0 +1,10 @@
+puts "Bonjour, quelle est ton année de naissance ?"
+print "> "
+user_birthyear = Integer(gets.chomp)
+user_age = user_birthyear - user_birthyear
+while user_birthyear < 2018
+  puts user_birthyear
+  user_birthyear = user_birthyear + 1
+  puts user_age
+  user_age = user_age + 1
+end
