9280ea910245e492b04fef1d17e379eeaa30d89e
Alexisreau97 <alexis.reaupro@gmail.com>
2018-08-30 18:27:14 +0200

    exo15

--- exo_15.rb
+++ exo_15.rb
@@ -1,7 +1,7 @@
 puts "Bonjour, quelle est ton année de naissance ?"
 print "> "
 user_birthyear = Integer(gets.chomp)
-user_age = user_birthyear - user_birthyear
+user_age = 0
 while user_birthyear < 2018
   puts user_birthyear
   user_birthyear = user_birthyear + 1
