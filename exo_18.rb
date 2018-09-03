7d057d66e3114f718f8fa57c2ed18db8e33a3fef
Alexisreau97 <alexis.reaupro@gmail.com>
2018-09-03 11:19:04 +0200

    exo18

---
+++ exo_18.rb
@@ -0,0 +1,16 @@
+email_number = 1
+email_array = []
+
+50.times do
+if email_number < 10
+	email = "jean.dupont.0#{email_number}@email.fr"
+	email_array << email
+	email_number = email_number + 1
+else
+	email = "jean.dupont.#{email_number}@email.fr"
+	email_array << email
+	email_number = email_number + 1
+end
+end
+
+puts email_array
